919ef9a49b36a7ac1ce35978a57def64e7b14e9d
s9general <sean.west1@gmail.com>
2022-02-17 09:56:27 +1100

    Create milestokilometers.py

---
+++ milestokilometers.py
@@ -0,0 +1,8 @@
+#Convert miles to kilometers
+
+#Prompt the user for the number of miles
+miles = input("How many miles? ")
+#convert miles to kilometers, convert it to a float first
+kilometers = float(miles) * 1.609
+#Concatonate then print the answer
+print(str(miles) + " miles equals "+ str(kilometers) + " kilometers." )
